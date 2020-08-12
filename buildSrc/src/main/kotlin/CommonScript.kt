ad6bd631e26b6e087b66f28923b0ea55026ba610
yt8492 <tmymyut2000@gmail.com>
2020-08-12 23:26:30 +0900

    add common scripts

---
+++ buildSrc/src/main/kotlin/CommonScript.kt
@@ -0,0 +1,40 @@
+import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.api.plugins.ExtensionAware
+
+private fun Project.androidExt(configure: BaseAppModuleExtension.() -> Unit) {
+    (this as ExtensionAware).extensions.configure("android", configure)
+}
+
+fun Project.androidCommon() {
+    androidExt {
+        compileSdkVersion(Android.compileSdk)
+        buildToolsVersion = Android.buildTools
+        aaptOptions.cruncherEnabled = false
+
+        defaultConfig {
+            minSdkVersion(Android.minSdk)
+            targetSdkVersion(Android.targetSdk)
+            versionCode = Android.versionCode
+            versionName = Android.versionName
+            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
+        buildTypes {
+            getByName("release") {
+                isMinifyEnabled = false
+                proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            }
+        }
+        dataBinding {
+            isEnabled = true
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+    }
+}
